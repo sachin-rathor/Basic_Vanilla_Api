e7d4bb441d56f9e209c26249780bd3861aa39bd0
Sachin Rathor <85805736+sachin-rathor@users.noreply.github.com>
2023-01-05 00:50:53 +0530

    Update index.js

--- api/HttpTrigger/index.js
+++ api/HttpTrigger/index.js
@@ -1,7 +1,7 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const responseMessage = "Hello from the Api"
+    const responseMessage = "Hello from the Sachin who enabled proxy setting...!!!"
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: {
